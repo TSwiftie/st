de4e797b371c4907f7004ac52f8a2a5de4d71858
TSwiftie <tswiftie@foxmail.com>
2021-04-11 00:11:35 +0000

    update

--- config.h
+++ config.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "FiraCode Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
+
+/* disable bold, italic and roman fonts globally */
+int disablebold = 0;
+int disableitalic = 0;
+int disableroman = 0;
+
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
@@ -104,42 +110,72 @@ unsigned int tabspaces = 4;
 float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
+/*
+ *static const char *colorname[] = {
+ *
+ *  [> 8 normal colors <]
+ *  [0] = "#000000", [> black   <]
+ *  [1] = "#ff5555", [> red     <]
+ *  [2] = "#50fa7b", [> green   <]
+ *  [3] = "#f1fa8c", [> yellow  <]
+ *  [4] = "#bd93f9", [> blue    <]
+ *  [5] = "#ff79c6", [> magenta <]
+ *  [6] = "#8be9fd", [> cyan    <]
+ *  [7] = "#bbbbbb", [> white   <]
+ *
+ *  [> 8 bright colors <]
+ *  [8]  = "#44475a", [> black   <]
+ *  [9]  = "#ff5555", [> red     <]
+ *  [10] = "#50fa7b", [> green   <]
+ *  [11] = "#f1fa8c", [> yellow  <]
+ *  [12] = "#bd93f9", [> blue    <]
+ *  [13] = "#ff79c6", [> magenta <]
+ *  [14] = "#8be9fd", [> cyan    <]
+ *  [15] = "#ffffff", [> white   <]
+ *
+ *  [> special colors <]
+ *  [256] = "#282a36", [> background <]
+ *  [257] = "#f8f8f2", [> foreground <]
+ *  "#282a36",
+ *};
+ */
+ /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-
-  /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
-
-  /* special colors */
-  [256] = "#282a36", /* background */
-  [257] = "#f8f8f2", /* foreground */
-  "#282a36",
-};
+ 	/* 8 normal colors */
+	"#3b4252", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#88c0d0", /* cyan    */
+	"#e5e9f0", /* white   */
+ 
+ 	/* 8 bright colors */
+	"#4c566a", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#8fbcbb", /* cyan    */
+	"#eceff4", /* white   */
+ 
+ 	[255] = 0,
+ 
+ 	/* more colors can be added after 255 to use with DefaultXX */
+	"#2e3440", /* background */
+	"#d8dee9", /* foreground */
+ };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 258;
+unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
--- x.c
+++ x.c
@@ -1031,7 +1031,11 @@ xloadfonts(char *fontstr, double fontsize)
 	win.ch = ceilf(dc.font.height * chscale);
 
 	FcPatternDel(pattern, FC_SLANT);
-	FcPatternAddInteger(pattern, FC_SLANT, FC_SLANT_ITALIC);
+	//FcPatternAddInteger(pattern, FC_SLANT, FC_SLANT_ITALIC);
+    if (!disableitalic)
+		FcPatternAddInteger(pattern, FC_SLANT, FC_SLANT_ITALIC);
+	if (!disableroman)
+		FcPatternAddInteger(pattern, FC_SLANT, FC_SLANT_ROMAN);
 	if (xloadfont(&dc.ifont, pattern))
 		die("can't open font %s\n", fontstr);
 
@@ -1528,9 +1532,10 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
 	}
 
 	/* Change basic system colors [0-7] to bright system colors [8-15] */
+    /*
 	if ((base.mode & ATTR_BOLD_FAINT) == ATTR_BOLD && BETWEEN(base.fg, 0, 7))
 		fg = &dc.col[base.fg + 8];
-
+    */
 	if (IS_SET(MODE_REVERSE)) {
 		if (fg == &dc.col[defaultfg]) {
 			fg = &dc.col[defaultbg];
