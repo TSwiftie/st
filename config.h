b54de388521c8ed7c79dc709b2bd126da40ca727
TSwiftie <tswiftie@foxmail.com>
2021-05-10 19:17:43 +0000

    update font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Fira Code Nerd Font Mono:pixelsize=22:antialias=true:autohint=true";
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 1;
@@ -16,7 +16,7 @@ int disableroman = 0;
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-    "Fira Code Nerd Font Mono:pixelsize=20:antialias=true:autohint=true",
+    "Fira Code Nerd Font Mono:pixelsize=22:antialias=true:autohint=true",
 };
 
 static int borderpx = 0;
@@ -110,63 +110,63 @@ unsigned int tabspaces = 4;
 float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+
+  /* 8 normal colors */
+  [0] = "#000000", /* black   */
+  [1] = "#ff5555", /* red     */
+  [2] = "#50fa7b", /* green   */
+  [3] = "#f1fa8c", /* yellow  */
+  [4] = "#bd93f9", /* blue    */
+  [5] = "#ff79c6", /* magenta */
+  [6] = "#8be9fd", /* cyan    */
+  [7] = "#bbbbbb", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#44475a", /* black   */
+  [9]  = "#ff5555", /* red     */
+  [10] = "#50fa7b", /* green   */
+  [11] = "#f1fa8c", /* yellow  */
+  [12] = "#bd93f9", /* blue    */
+  [13] = "#ff79c6", /* magenta */
+  [14] = "#8be9fd", /* cyan    */
+  [15] = "#ffffff", /* white   */
+
+  /* special colors */
+  [256] = "#282a36", /* background */
+  [257] = "#f8f8f2", /* foreground */
+  "#282a36",
+};
+ /* Terminal colors (16 first used in escape sequence) */
 /*
  *static const char *colorname[] = {
- *
- *  [> 8 normal colors <]
- *  [0] = "#000000", [> black   <]
- *  [1] = "#ff5555", [> red     <]
- *  [2] = "#50fa7b", [> green   <]
- *  [3] = "#f1fa8c", [> yellow  <]
- *  [4] = "#bd93f9", [> blue    <]
- *  [5] = "#ff79c6", [> magenta <]
- *  [6] = "#8be9fd", [> cyan    <]
- *  [7] = "#bbbbbb", [> white   <]
- *
- *  [> 8 bright colors <]
- *  [8]  = "#44475a", [> black   <]
- *  [9]  = "#ff5555", [> red     <]
- *  [10] = "#50fa7b", [> green   <]
- *  [11] = "#f1fa8c", [> yellow  <]
- *  [12] = "#bd93f9", [> blue    <]
- *  [13] = "#ff79c6", [> magenta <]
- *  [14] = "#8be9fd", [> cyan    <]
- *  [15] = "#ffffff", [> white   <]
- *
- *  [> special colors <]
- *  [256] = "#282a36", [> background <]
- *  [257] = "#f8f8f2", [> foreground <]
- *  "#282a36",
- *};
+ * 	[> 8 normal colors <]
+ *    "#3b4252", [> black   <]
+ *    "#bf616a", [> red     <]
+ *    "#a3be8c", [> green   <]
+ *    "#ebcb8b", [> yellow  <]
+ *    "#81a1c1", [> blue    <]
+ *    "#b48ead", [> magenta <]
+ *    "#88c0d0", [> cyan    <]
+ *    "#e5e9f0", [> white   <]
+ * 
+ * 	[> 8 bright colors <]
+ *    "#4c566a", [> black   <]
+ *    "#bf616a", [> red     <]
+ *    "#a3be8c", [> green   <]
+ *    "#ebcb8b", [> yellow  <]
+ *    "#81a1c1", [> blue    <]
+ *    "#b48ead", [> magenta <]
+ *    "#8fbcbb", [> cyan    <]
+ *    "#eceff4", [> white   <]
+ * 
+ * 	[255] = 0,
+ * 
+ * 	[> more colors can be added after 255 to use with DefaultXX <]
+ *    "#2e3440", [> background <]
+ *    "#d8dee9", [> foreground <]
+ * };
  */
- /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
- 	/* 8 normal colors */
-	"#3b4252", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#88c0d0", /* cyan    */
-	"#e5e9f0", /* white   */
- 
- 	/* 8 bright colors */
-	"#4c566a", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#8fbcbb", /* cyan    */
-	"#eceff4", /* white   */
- 
- 	[255] = 0,
- 
- 	/* more colors can be added after 255 to use with DefaultXX */
-	"#2e3440", /* background */
-	"#d8dee9", /* foreground */
- };
 
 /*
  * Default colors (colorname index)
@@ -175,7 +175,7 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
